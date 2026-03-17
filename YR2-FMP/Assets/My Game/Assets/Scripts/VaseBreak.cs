8534e64f86a0283ebd65cea42104a67cb0b63bce
Charlie Hale <147997731+CJPro25@users.noreply.github.com>
2026-03-17 12:14:02 +0000

    First Prototype Scene

--- YR2-FMP/Assets/My Game/Assets/Scripts/VaseBreak.cs
+++ YR2-FMP/Assets/My Game/Assets/Scripts/VaseBreak.cs
@@ -3,26 +3,22 @@ public class VaseBreak : MonoBehaviour
 {
     public GameObject vase;
     Vector3 lastvelocity;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Update()
     {
-        if(GetComponent<Rigidbody>().linearVelocity != new Vector3(0, 0, 0))
-        {
-            lastvelocity = GetComponent<Rigidbody>().linearVelocity;
-        }
+        //This part adds the current velocity to lastvelocity variable which is then used later on
+        if(GetComponent<Rigidbody>().linearVelocity != new Vector3(0, 0, 0)){lastvelocity = GetComponent<Rigidbody>().linearVelocity;}
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Breaker")
         {
-            vase = Instantiate(vase, transform.position, transform.rotation);
-            Debug.Log(vase);
-            foreach (Rigidbody t in vase.transform.GetComponentsInChildren<Rigidbody>())
+            vase = Instantiate(vase, transform.position, transform.rotation); //Adds the broken vase in the same position as the original vase
+
+            foreach (Rigidbody t in vase.transform.GetComponentsInChildren<Rigidbody>()) //For every rigidbody child of the broken vase
             {
-                t.AddForce(lastvelocity * GetComponent<Rigidbody>().mass * 10);
-                Debug.Log(t.linearVelocity);
+                t.AddForce(lastvelocity * GetComponent<Rigidbody>().mass * 10); //Adds the original velocity to the broken pieces
             }
-            Destroy(gameObject);
+            Destroy(gameObject); //Destroys the original vase
         }
     }
 }
